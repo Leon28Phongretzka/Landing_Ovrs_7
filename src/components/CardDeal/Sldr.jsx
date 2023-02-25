566a8c16fc2530dd38a58d1f7db393f13506d72f
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-02-25 14:56:04 +0700

    25 02 23

--- src/components/CardDeal/Sldr.jsx
+++ src/components/CardDeal/Sldr.jsx
@@ -19,14 +19,14 @@ const SimpleSlider = ({ slides }) => {
     pauseOnHover: true,
     responsive: [
       {
-        breakpoint: 1000,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
